fafee9550bfd4af6f5313e8c3aea6623ddc0bd8d
Julius Kiano <jgkiano@gmail.com>
2021-07-02 07:29:40 +0300

    added initialize

--- ios/ReactNativeCore.m
+++ ios/ReactNativeCore.m
@@ -2,8 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(ReactNativeCore, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(initialize:(NSString *)branchId clientKey:(NSString *)clientKey environment:(NSString *)environment resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
 @end
--- ios/ReactNativeCore.swift
+++ ios/ReactNativeCore.swift
@@ -1,8 +1,21 @@
+import Foundation
+
 @objc(ReactNativeCore)
 class ReactNativeCore: NSObject {
-
+    
+    var credentials: NSDictionary?
+    
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         resolve(a*b)
+    }
+    
+    @objc func initialize(_ branchId: String, clientKey: String, environment: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        credentials = [
+            "branchId": branchId,
+            "clientKey": clientKey,
+            "environment": environment
+        ]
+        resolve(true)
     }
 }
