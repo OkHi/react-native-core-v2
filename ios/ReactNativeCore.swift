ba903f66375a586463636fa0804bab6cdd0ae326
Julius Kiano <jgkiano@gmail.com>
2021-07-02 08:39:47 +0300

    added check methods for ios

--- ios/ReactNativeCore.swift
+++ ios/ReactNativeCore.swift
@@ -1,21 +1,39 @@
-import Foundation
+import UIKit
+import OkCore
 
 @objc(ReactNativeCore)
 class ReactNativeCore: NSObject {
     
     var credentials: NSDictionary?
     
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
+    let okhiLocationService = OkHiLocationService()
     
     @objc func initialize(_ branchId: String, clientKey: String, environment: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
-        credentials = [
-            "branchId": branchId,
-            "clientKey": clientKey,
-            "environment": environment
-        ]
+        credentials = [ "branchId": branchId, "clientKey": clientKey, "environment": environment ]
         resolve(true)
+    }
+    
+    @objc func isLocationPermissionGranted(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        resolve(okhiLocationService.isLocationPermissionGranted())
+    }
+    
+    @objc func isBackgroundLocationPermissionGranted(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        resolve(okhiLocationService.isLocationPermissionGranted()) // TODO: need isBackgroundLocationPermissionGranted
+    }
+    
+    @objc func isLocationServicesEnabled(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        resolve(okhiLocationService.isLocationServicesAvailable())
+    }
+    
+    @objc func openLocationServicesSettings (_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)
+    }
+    
+    @objc func requestEnableLocationServices (_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)
+    }
+    
+    @objc func getSDKVersion(_ resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) {
+        resolve(UIDevice.current.systemVersion)
     }
 }
